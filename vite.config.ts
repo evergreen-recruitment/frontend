7aeb892a2787a4c24b6fb7cf87e85b5dcabf2c56
pepedd864 <pepedd@qq.com>
2024-05-08 23:52:41 +0800

    chore: 引入jsx

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -17,6 +18,7 @@ export default defineConfig(({ command, mode }) => {
   return {
     plugins: [
       vue(),
+      vueJsx(),
       eslintPlugin(),
       // 图片压缩
       // vsharp(),
